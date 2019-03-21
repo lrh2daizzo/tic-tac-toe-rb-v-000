138f429f3af91376127854027e55fc54de4dcf9a
Luis Herrera <herrera.lrh@gmail.com>
2019-03-21 01:26:24 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -78,3 +78,12 @@ def winner(board)
     board[winning_combo.first]
   end
 end
+
+def play(board)
+  turn(board) until over?(board)
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+end
